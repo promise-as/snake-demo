15f6d41f3b5b8c4007542a82ff3f4af352a47cc2
Hong Baojin <13809735212.163.com>
2020-05-26 18:32:10 +0800

    Write it again

--- index.js
+++ index.js
@@ -1,42 +1,23 @@
-// 在刷新页面时不仅要产生地图，还要产生两个小div
-// feng别对应贪吃蛇的 蛇头 以及身体
-// 由于蛇头(红色) 食物(蓝色) 以及多个身体(黄色) 都是动态创建的div
-// 封装一个方法 用于创建div元素 放入地图里
-// 参数可以拓展函数 功能
 var map = document.getElementById('map');
-var bodyNodes = []; // 放置所有身体的
+var bodyNodes = []; // 放置所有身体
 var Nodes = []; // 放置整个蛇
+// 统计分数
+var scoreEle = document.querySelector('#score');
 function createDiv(color) {
-  // 创建一个div节点
   var div = document.createElement('div');
-  // 位置是随机产生的 js随机数
-  // Math.random()
-  // 0-450之间 50的倍数 0-9倍
-  // Math.random() 产生一个0-1的随机数(小数) 不包含1 [0, 1)
-  // 产生一个0-9的随机数 parseInt(Math.random() * 10)
   div.style.left = parseInt(Math.random() * 10) * 50 + 'px';
   div.style.top = parseInt(Math.random() * 10) * 50 + 'px';
   div.style.background = color;
   // 添加到地图中
   map.appendChild(div);
-  // return 就是函数执行后的返回结果 等于函数所创建出来的div
   return div;
 }
-// 如何让蛇头移动起来 操作蛇头 js找到蛇头对应的div
-// 蛇头移动 如何让蛇头移动起来 四个方向
-// 移动前需要判断一下当前蛇头是要向哪个方向进行移动
-// .value 索引值 左 右 上 下
-// 假设蛇头的默认移动方向是向左
-var headNode = createDiv('red'); // 创建一个蛇头
-Node.push(headNode);
-headNode.value = '右'; // .value是判断蛇头的移动方向
-var foodNode = createDiv('blue'); // 创建食物
+var headNode = createDiv('red');
+Nodes.push(headNode);
+headNode.value = '右';
+var foodNode = createDiv('blue');
 function move() {
   if (bodyNodes.length > 0) {
-    // 身体移动通过循环遍历让身体移动起来
-    // 如何让身体跟随头部移动???
-    // 身体跟随前一块移动 让他走上一块上一次行走的方向
-    // 反向循环为什么就可以了?
     for (var i = bodyNodes.length - 1; i >= 0; i--) {
       switch (bodyNodes[i].value) {
         case '左':
@@ -45,18 +26,11 @@ function move() {
         case '右':
           bodyNodes[i].style.left = parseInt(bodyNodes[i].style.left) + 50 + 'px';
           break;
-        case '上':
-          bodyNodes[i].style.top = parseInt(bodyNodes[i].style.top) - 50 + 'px';
-          break;
-        case '右':
-          bodyNodes[i].style.top = parseInt(bodyNodes[i].style.top) + 50 + 'px';
-          break;
       }
-      // bodyNodes[0] headNode 上一次的方向
       if (i == 0) {
-        bodyNodes[i].value = headNode
+        bodyNodes[i].value = headNode.value;
       } else {
-        bodyNodes[i].value = bodyNodes[i - 1].value
+        bodyNodes[i].value = bodyNodes[i - 1].value;
       }
     }
   }
@@ -71,10 +45,51 @@ function move() {
     case '上':
       headNode.style.top = parseInt(headNode.style.top) - 50 + 'px';
       break;
-    case '右':
-      headNode.style.top = parseInt(headNode.style.top) - 50 + 'px';
+    case '下':
+      headNode.style.top = parseInt(headNode.style.top) + 50 + 'px';
       break;
   }
   // 判断是否出边界
-  
+  if (parseInt(headNode.style.left) < 0 || parseInt(headNode.style.left) > 450
+    || parseInt(headNode.style.top) < 0 || parseInt(headNode.style.top) > 450) {
+    clearInterval(t);
+    alert('撞墙了')
+  }
+  // 判断是否咬到身体
+  if (bodyNodes.length > 0) {
+    for (var i = 0; i < bodyNodes.length; i++) {
+      if (headNode.style.left === bodyNodes[i].style.left && headNode.style.top === bodyNodes[i].style.top) {
+        clearInterval(t);
+        alert('咬到身体了')
+      }
+    }
+  }
+  // 碰撞检测
+  if (headNode.style.left === foodNode.style.left && headNode.style.top === foodNode.style.top) {
+    var bodyNode = createDiv('yellow');
+    var lastNode = null;
+    if (bodyNodes.length > 0) {
+      lastNode = bodyNodes[bodyNodes.length - 1];
+    } else {
+      lastNode = headNode;
+    }
+  }
+  switch (lastNode.value) {
+    case '左':
+      bodyNode.style.left = parseInt(lastNode.style.left) + 50 + 'px';
+      bodyNode.style.top = lastNode.style.top;
+      break
+    case '右':
+      bodyNode.style.left = parseInt(lastNode.style.left) - 50 + 'px';
+      bodyNode.style.top = lastNode.style.top;
+      break
+    case '上':
+      bodyNode.style.top = parseInt(lastNode.style.top) + 50 + 'px';
+      bodyNode.style.left = lastNode.style.left;
+      break
+    case '下':
+      bodyNode.style.top = parseInt(lastNode.style.top) - 50 + 'px';
+      bodyNode.style.left = lastNode.style.left;
+      break
+  }
 }
